cc06f2fc507364e3824e9a750d3cd7ccef9e8495
fazl09927194048-art <fazl09927194048@gmail.com>
2026-09-15 08:36:21 +0330

    Bump xXx DRO Android app version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.xxx.superapp"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
     }
 }
 
